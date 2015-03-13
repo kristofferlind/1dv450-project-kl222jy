7148e35b79a4c3b9f53d4b1e4358c2b985099d32
kristofferlind <kristoffer@krad.se>
2015-03-13 12:03:07 +0100

    fix: auth logic, different domains

--- project/app/controllers/home_controller.rb
+++ project/app/controllers/home_controller.rb
@@ -8,7 +8,11 @@ class HomeController < ApplicationController
     payload = Hash.new
     payload[:creatorId] = @creator.id
     payload[:exp] = 2.hours.from_now.to_i()
-    cookies[:token] = JWT.encode(payload, Rails.application.secrets.app_secret, "HS512")
-    redirect_to(root_path)
+    @token = JWT.encode(payload, Rails.application.secrets.app_secret, "HS512")
+    cookies[:token] = @token
+    # redirect_to(root_path)
+    # @origin = request.env['omniauth.origin'] + 'callback?token=' + @token
+    @origin = request.env['omniauth.origin'] + '#/token/' + @token
+    redirect_to @origin || root_path
   end
 end
